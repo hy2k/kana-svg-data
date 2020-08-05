340997a8aaf27e19518a2d17aa62a2ed4520851a
Yusuke Higuchi <hy2k@outlook.jp>
2020-08-05 17:44:40 +0900

    configure eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-unsafe-call': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+  },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import * as fs from "fs";
-import * as path from "path";
-import { parse as parseSVG } from "svgson";
+import * as fs from 'fs'
+import * as path from 'path'
+import { parse as parseSVG } from 'svgson'
 
-const inputDir = path.join(__dirname, "../vendor/animCJK/svgsKana");
-const distDir = path.join(__dirname, "../dist");
+const inputDir = path.join(__dirname, '../vendor/animCJK/svgsKana')
+const distDir = path.join(__dirname, '../dist')
 
-type KanaCategoty = "hiragana" | "katakana"
+type KanaCategoty = 'hiragana' | 'katakana'
 type KanaDictionary = {
-  [key in KanaCategoty]: string[];
-};
+  [key in KanaCategoty]: string[]
+}
+
 // prettier-ignore
 const kanaDict: KanaDictionary = {
   hiragana: [
@@ -37,99 +38,97 @@ const kanaDict: KanaDictionary = {
   ],
 }
 
-async function onFileContent(
-  fileContent: string
-) {
-  const svgNode = await parseSVG(fileContent);
+async function onFileContent(fileContent: string) {
+  const svgNode = await parseSVG(fileContent)
 
   // Remove the prefix `z` from id
-  const id = svgNode.attributes.id.slice(1);
+  const id = svgNode.attributes.id.slice(1)
 
   const allSvgPaths = svgNode.children
-    .filter((el) => el.name === "path")
-    .map((el) => el.attributes);
+    .filter((el) => el.name === 'path')
+    .map((el) => el.attributes)
 
   const stroke = allSvgPaths
     .filter((el) => el.id)
     .map((el) => ({
-      id: el.id.replace(/.*d/, ""),
+      id: el.id.replace(/.*d/, ''),
       d: el.d,
-    }));
+    }))
 
   const median = allSvgPaths
-    .filter((el) => el["clip-path"])
+    .filter((el) => el['clip-path'])
     .map((el) => ({
-      id: el["clip-path"].replace(/.*c(\d+[a-z]*).+/, "$1"),
+      id: el['clip-path'].replace(/.*c(\d+[a-z]*).+/, '$1'),
       d: el.d,
-    }));
+    }))
 
   return {
     id,
     stroke,
     median,
-  };
+  }
 }
 
 function classify(dir: string, dict: KanaDictionary) {
-  const svgFiles = new Map<string, string[]>();
-  const allFilenames = fs.readdirSync(dir);
+  const svgFiles = new Map<string, string[]>()
+  const allFilenames = fs.readdirSync(dir)
 
-  Object.keys(dict).forEach((key: KanaCategoty) => {
+  Object.keys(dict).forEach((key) => {
     const filenames = allFilenames.filter((filename) => {
-      const charcode = parseInt(path.basename(filename, ".svg"));
-      return dict[key].includes(String.fromCharCode(charcode));
-    });
+      const charcode = parseInt(path.basename(filename, '.svg'))
+      return dict[key as KanaCategoty].includes(String.fromCharCode(charcode))
+    })
 
-    svgFiles.set(key, filenames);
-  });
+    svgFiles.set(key, filenames)
+  })
 
-  return svgFiles;
+  return svgFiles
 }
 
 if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir);
+  fs.mkdirSync(distDir)
 }
 
 classify(inputDir, kanaDict).forEach((filelist, key) => {
-  const outDir = path.join(distDir, key);
+  const outDir = path.join(distDir, key)
   if (!fs.existsSync(outDir)) {
-    fs.mkdirSync(outDir);
+    fs.mkdirSync(outDir)
   }
 
   const promises = filelist.map((filename) => {
     const buffer = fs.readFileSync(path.resolve(inputDir, filename), {
-      encoding: "utf-8",
-    });
+      encoding: 'utf-8',
+    })
 
-    return onFileContent(buffer);
-  });
+    return onFileContent(buffer)
+  })
 
   const outPathAllChars = path.join(
     distDir,
     `all${key.charAt(0).toUpperCase() + key.slice(1)}.json`
-  );
+  )
 
   Promise.all(promises)
     .then((data) => {
       fs.writeFile(outPathAllChars, JSON.stringify(data, null, 2), (err) => {
         if (err) {
-          console.log(err);
+          console.log(err)
         }
-      });
+      })
 
       data.forEach((item) => {
-        const jaChar = String.fromCharCode(parseInt(item.id));
+        const jaChar = String.fromCharCode(parseInt(item.id))
 
-        const outPathChar = path.join(outDir, `${jaChar}.json`);
+        const outPathChar = path.join(outDir, `${jaChar}.json`)
 
         fs.writeFile(outPathChar, JSON.stringify(item, null, 2), (err) => {
           if (err) {
-            console.log(err);
+            console.log(err)
           }
-        });
-      });
+        })
+      })
     })
     .catch((err) => {
-      console.error(err);
-    });
-});
+      console.error(err)
+    })
+})
